35545606d34f9e59000ab115f9fbadff5ab303c1
Carlos Ruiz <carlos.ruiz.dominguez@west.cmu.edu>
2018-04-10 16:17:53 +0200

    Minor improvement on camera FPS estimation

--- MyCode/opencv_find_camera_FPS.py
+++ MyCode/opencv_find_camera_FPS.py
@@ -8,14 +8,14 @@ import os
 from datetime import datetime, timedelta
 
 
-def find_FPS(cam_id=0, test_len=10.0, saveimg=False, saveimgFolder="fps_test"):
+def find_FPS(cam_id=0, test_len=10.0, showimg=True, saveimg=False, saveimgFolder="fps_test"):
 	video_capture = cv2.VideoCapture(cam_id)
 
 	fps = video_capture.get(cv2.CAP_PROP_FPS)
 	print "OpenCV says FPS is {:.2f}".format(fps)
 
-	if fps > 0:
-		return fps
+	# if fps > 0:
+	# 	return fps
 	if not video_capture.isOpened():
 		print "Couldn't open camera! :("
 		return -1
@@ -26,7 +26,7 @@ def find_FPS(cam_id=0, test_len=10.0, saveimg=False, saveimgFolder="fps_test"):
 			os.makedirs(saveimgFolder)
 
 	cnt_diff_frames = 0
-	last_frame = np.zeros((video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT), video_capture.get(cv2.CAP_PROP_FRAME_WIDTH), 3))
+	last_frame = np.zeros((int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)), 3))
 	startT = datetime.now()
 	endT = startT + timedelta(seconds=test_len)
 	while (datetime.now() < endT):
@@ -39,6 +39,9 @@ def find_FPS(cam_id=0, test_len=10.0, saveimg=False, saveimgFolder="fps_test"):
 		tt = datetime.now()
 		# imgDiffPx = np.any(frame != last_frame, 2)
 		nDiffPx = 1
+		if showimg:
+			cv2.imshow('Output', frame)
+			cv2.waitKey(1)
 		if saveimg:
 			imgDiffPx = (frame == last_frame)
 			nDiffPx = np.sum(imgDiffPx)
@@ -47,7 +50,7 @@ def find_FPS(cam_id=0, test_len=10.0, saveimg=False, saveimgFolder="fps_test"):
 		if nDiffPx > 0:
 			cnt_diff_frames += 1
 			strAdd = " - New frame detected!"
-		print "At {} (took {}|{}): {:7} different pixels{}".format(str(t-startT)[:-3], str(tt-t)[6:-3], str(datetime.now()-tt)[6:-3], nDiffPx, strAdd)
+		print("At {} (took {}|{}): {:7} different pixels{}".format(str(t-startT)[:-3], str(tt-t)[6:-3], str(datetime.now()-tt)[6:-3], nDiffPx, strAdd))
 		# print "{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}".format(video_capture.get(cv2.CAP_PROP_APERTURE), video_capture.get(cv2.CAP_PROP_AUTO_EXPOSURE), video_capture.get(cv2.CAP_PROP_BACKLIGHT), video_capture.get(cv2.CAP_PROP_BRIGHTNESS), video_capture.get(cv2.CAP_PROP_CONTRAST), video_capture.get(cv2.CAP_PROP_EXPOSURE), video_capture.get(cv2.CAP_PROP_EXPOSUREPROGRAM), video_capture.get(cv2.CAP_PROP_FRAME_COUNT), video_capture.get(cv2.CAP_PROP_FOCUS), video_capture.get(cv2.CAP_PROP_GAIN), video_capture.get(cv2.CAP_PROP_GAMMA), video_capture.get(cv2.CAP_PROP_IRIS), video_capture.get(cv2.CAP_PROP_ISO_SPEED), video_capture.get(cv2.CAP_PROP_OPENNI_FOCAL_LENGTH), video_capture.get(cv2.CAP_PROP_POS_FRAMES), video_capture.get(cv2.CAP_PROP_POS_MSEC), video_capture.get(cv2.CAP_PROP_ROLL), video_capture.get(cv2.CAP_PROP_SATURATION), video_capture.get(cv2.CAP_PROP_SHARPNESS), video_capture.get(cv2.CAP_PROP_SPEED), video_capture.get(cv2.CAP_PROP_TEMPERATURE), video_capture.get(cv2.CAP_PROP_WHITE_BALANCE_BLUE_U))
 		last_frame = frame
 
